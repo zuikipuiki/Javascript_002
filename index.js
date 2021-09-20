864b80e6e7518066d930851b804e22dff1f53560
zuikipuiki <31111953+zuikipuiki@users.noreply.github.com>
2021-09-20 09:59:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Stukje text
+console.log('Hallo Winc Academy studenten');
+
+// Berekening van een grote som
+/* let multiplication = 1230941 * 1823794;
+console.log("multiplication:", multiplication);
+
+let division = 637263 / 54;
+console.log("division:", division); */
